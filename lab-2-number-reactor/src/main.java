829fd10218b8bb1dd427df0afe970e8724026cb6
shinabarger <davidshinabarger@gmail.com>
2017-01-31 15:21:41 -0500

    reformatted & cleaned code

--- lab-2-number-reactor/src/main.java
+++ lab-2-number-reactor/src/main.java
@@ -12,8 +12,6 @@ public class main {
         System.out.println("Hello, " + userName + ". Nice to meet you.");
 
 
-
-
         //TODO add do/while loop here
         do {
             //check and see if user# is within range
@@ -22,23 +20,23 @@ public class main {
             int userNumber = scan.nextInt();
             scan.nextLine();
             if (userNumber > 100 || userNumber < 1) {
-            System.out.println("Oops! That wasn't a # between 1 & 100!");
+                System.out.println("Oops! That wasn't a # between 1 & 100!");
                 System.out.println("Please enter another number: ");
                 userNumber = scan.nextInt();
                 scan.nextLine();
-        }
+            }
 
-        if (userNumber % 2 == 0 & userNumber > 25) {
-            System.out.print("Thanks, " + userName + ". Your number was even. ");
-        } else if (userNumber <= 25) {
-            System.out.println("Thanks. Your number was even and less than 25.");
-        }
+            if (userNumber % 2 == 0 & userNumber > 25) {
+                System.out.print("Thanks, " + userName + ". Your number was even. ");
+            } else if (userNumber <= 25) {
+                System.out.println("Thanks. Your number was even and less than 25.");
+            }
 
-        if (userNumber % 2 != 0) {
-            System.out.print("Thanks, " + userName + ". Your number was odd. ");
-        } else if (userNumber % 2 != 0 & userNumber > 60) {
-            System.out.println("Thanks your number is odd and greater than 60.");
-        }
+            if (userNumber % 2 != 0) {
+                System.out.print("Thanks, " + userName + ". Your number was odd. ");
+            } else if (userNumber % 2 != 0 & userNumber > 60) {
+                System.out.println("Thanks your number is odd and greater than 60.");
+            }
             System.out.println("Would you like to play again? Type yes or no: ");
             userinput = scan.nextLine();
         } while (userinput.equalsIgnoreCase("yes"));
